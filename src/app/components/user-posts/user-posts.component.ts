6d64cabbfd41fa12c1989cd7fc3ae2410bd280a0
Anna Melnyk <blairw73@gmail.com>
2024-09-25 09:59:40 +1000

    Remove loader

--- src/app/components/user-posts/user-posts.component.ts
+++ src/app/components/user-posts/user-posts.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core'
+import { AsyncPipe, NgTemplateOutlet } from '@angular/common'
 import { ActivatedRoute, Router } from '@angular/router'
-import { distinctUntilChanged } from 'rxjs'
+import { distinctUntilChanged, Observable, Subject } from 'rxjs'
 
 import { BackendService } from '../../services/backend.service'
+import { Post } from '../../services/types'
 
 @Component({
   selector: 'app-user-posts',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgTemplateOutlet],
   templateUrl: './user-posts.component.html',
   styleUrl: './user-posts.component.scss'
 })
 export class UserPostsComponent {
+  private postsSubject = new Subject<Post[]>()
+  posts$: Observable<Post[]> = this.postsSubject.asObservable()
+  isLoading = false
 
   constructor(
     private router: Router,
@@ -20,13 +25,19 @@ export class UserPostsComponent {
   ) { }
 
   ngOnInit() {
+    this.isLoading = true
     this.activatedRoute.paramMap.subscribe(params => {
       const userId = params.get('id') as string
       console.log(userId)
     })
 
-    const posts$ = this.backendService.getPostsList()
-    posts$.pipe(distinctUntilChanged()).subscribe(p => console.log(p))
+    this.backendService.getPostsList()
+    .pipe(distinctUntilChanged()).subscribe(p => {
+      this.isLoading = false
+      console.log(p)
+      this.postsSubject.next(p)
+
+    })
   }
 
 }
